3f346139bd0127bcd0223b5ac3f4d6fccb03ae86
Dennis Ritter <dennis.ritter@rms-consult.de>
2024-05-30 16:00:43 +0200

    Remove superfluous properties in ReportConfig

--- src/main/java/de/rms_consult/sqlh2erroronupgradeto3_3_0/ReportConfig.java
+++ src/main/java/de/rms_consult/sqlh2erroronupgradeto3_3_0/ReportConfig.java
@@ -8,13 +8,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.time.ZonedDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 /** Entity for a report configuration. */
 @Entity
@@ -29,17 +26,6 @@ public class ReportConfig {
   @Column(name = "id")
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
-//  @Column(name = "created")
-//  @DateTimeFormat(iso = ISO.DATE_TIME)
-//  private ZonedDateTime created;
-//
-//  @Column(name = "updated")
-//  @DateTimeFormat(iso = ISO.DATE_TIME)
-//  private ZonedDateTime updated;
-//
-//  @Column(name = "file_name")
-//  private String fileName;
 
   @Enumerated(EnumType.STRING)
   @Column(name = "period_type", columnDefinition = "varchar(255)")
